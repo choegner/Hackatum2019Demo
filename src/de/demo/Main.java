c60b1b417aab1bbc1f03fea68c2293a92c074abb
Christopher Hoegner <chris.hoegner@gmail.com>
2019-11-23 19:51:31 +0100

    Readme update

--- src/de/demo/Main.java
+++ src/de/demo/Main.java
@@ -3,6 +3,6 @@ package de.demo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
